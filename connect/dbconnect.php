bdcc61b1d7aa5cf4c3e3186800ca427a59c42713
Risha Sringa Changmai <michael.the.don@gmail.com>
2016-03-27 03:25:05 +0530

    initial commit

---
+++ connect/dbconnect.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelspeed
+ * Date: 3/27/16
+ * Time: 2:05 AM
+ */
+$serverName="localhost";
+$dbusername="root";
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: michaelspeed
+ * Date: 3/27/16
+ * Time: 2:02 AM
+ */
+
+if(isset($_REQUEST['submitBtn'])){
+
+}
