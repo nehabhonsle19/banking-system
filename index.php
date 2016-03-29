8dc4ddf9cebabc57212d8ae4107763ad6dae76e5
Risha Sringa Changmai <michael.the.don@gmail.com>
2016-03-29 21:45:21 +0530

    bootstrap added with navbar- centered. author- michael

--- index.php
+++ index.php
@@ -49,18 +49,32 @@ if (isset($_SESSION['customer_login']))
         <body>
         <div class="wrapper">
             <div class="header">
-
-            </div>
-            <div class="navbar">
-                <div class="container-fluid">
-                    <div class="navbar navbar-fixed-top">
-                        <div class="container">
-                            <div class="navbar-header">
-                                
-                            </div>
+                <nav class="navbar navbar-default" role="navigation">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                            <div class="navbar-brand navbar-brand-centered">MULTI-BANK</div>
+                        </div>
+                        <div class="collapse navbar-collapse" id="navbar-brand-centered">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Features</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                            </ul>
+                            <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Bank 1</a> </li>
+                                <li><a href="#">Bank 2</a> </li>
+                                <li><a href="#">Bank 3</a> </li>
+                            </ul>
                         </div>
                     </div>
-                </div>
+                </nav>
+
             </div>
         </div>
         </body>
