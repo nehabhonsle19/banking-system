7266be03f93fb31fdad3cdb4057a3b45e73961d2
Risha Sringa Changmai <michael.the.don@gmail.com>
2016-03-30 01:33:04 +0530

    index page complete - generated with bootstrap author- michael

--- index.php
+++ index.php
@@ -59,7 +59,7 @@ if (isset($_SESSION['customer_login']))
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <div class="navbar-brand navbar-brand-centered">MULTI-BANK</div>
+                            <div class="navbar-brand navbar-brand-centered">OSCAR</div>
                         </div>
                         <div class="collapse navbar-collapse" id="navbar-brand-centered">
                             <ul class="nav navbar-nav">
@@ -77,6 +77,73 @@ if (isset($_SESSION['customer_login']))
                 </nav>
                </div>
 
+            </div>
+
+            <div id="login-overlay" class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">X</span><span class="sr-only">Close</span> </button>
+                        <h4 class="modal-title" id="myModalLabel">Login To OSCAR</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <div class="well">
+                                    <form id="loginForm" method="POST" novalidate="novalidate">
+                                        <div class="form-group">
+                                            <label for="username" class="control-label">Username</label>
+                                            <input type="text" class="form-control" id="username" name="username" value="" required="" title="Please enter your username">
+                                            <span class="help-block"></span>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="password" class="control-label">Password</label>
+                                            <input type="password" class="form-control" id="password", name="password" value="" required="" title="Please enter your password">
+                                            <span class="help-block"></span>
+                                        </div>
+                                        <div id="loginErrorMsg" class="alert alert-error hide">Wrong username of password</div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember" id="remember"> Remember login
+                                            </label>
+                                            <p class="help-block">(if this is a private computer)</p>
+                                        </div>
+                                        <button type="submit" class="btn btn-success btn-block">Login</button>
+                                        <a href="/forgot/" class="btn btn-default btn-block">Help to login</a>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <p class="lead">Register now for a <span class="text-success">FREE</span> account </p>
+                                <ul class="list-unstyled" style="line-height: 2">
+                                    <li><span class="fa fa-check text-success"> Registration for online banking</span> </li>
+                                    <li><span class="fa fa-check text-success"> Adding Beneficiary account</span> </li>
+                                    <li><span class="fa fa-check text-success"> Funds Transfer</span> </li>
+                                    <li><span class="fa fa-check text-success"> Last Login record</span> </li>
+                                    <li><span class="fa fa-check text-success"> Mini Statment</span> </li>
+                                    <li><span class="fa fa-check text-success"> ATM and Cheque Book</span> </li>
+                                    <li><span class="fa fa-check text-success"> Account Statment by date</span> </li>
+                                </ul>
+                                <p><a href="/new-customer/" class="btn btn-info btn-block">Register Now</a> </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <div class="span2" style="width: 15%;">
+                            <ul class="unstyled">
+                                <li>Github</li>
+                                <li><a href="#">About us</a> </li>
+                                <li><a href="#">Contact & Support</a> </li>
+                            </ul>
+                        </div>
+                        <div class="span4">
+                            <p class="muted pull-right">© 2013 EntityDevelopers. All rights reserved</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         </body>
