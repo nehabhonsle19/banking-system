69d6772a99b3926e1def123206720b688173111a
Risha Sringa Changmai <michael.the.don@gmail.com>
2016-03-30 02:05:23 +0530

    added header and footer author- michael

---
+++ footer.php
@@ -0,0 +1,25 @@
+
+/**
+ * Created by PhpStorm.
+ * User: michaelspeed
+ * Date: 3/30/16
+ * Time: 2:03 AM
+ */
+<div class="footer">
+<div class="container">
+    <div class="row-fluid">
+        <div class="span12">
+            <div class="span2" style="width: 15%;">
+                <ul class="unstyled">
+                    <li>Github</li>
+                    <li><a href="#">About us</a> </li>
+                    <li><a href="#">Contact & Support</a> </li>
+                </ul>
+            </div>
+            <div class="span4">
+                <p class="muted pull-right">© 2013 EntityDevelopers. All rights reserved</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
