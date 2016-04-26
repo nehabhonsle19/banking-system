73f4e223adc24fc20b175c2e3ab07afe41c52bc3
Risha Sringa Changmai <michael.the.don@gmail.com>
2016-04-26 17:50:05 +0530

    modified safepnlinebanking

--- safonlinebanking.php
+++ safonlinebanking.php
@@ -29,11 +29,6 @@
         </ul>
         <h3 style="color:#2E4372;"><u>Beware of Phishing Attacks</u></h3>
         <ul>
-            <li>Phishing is a fraudulent attempt, usually made through email, phone calls, SMS etc seeking your personal and confidential information.</li>
-            <li>State Bank or any of its representative never sends you email/SMS or calls you over phone to get your personal information, password or one time SMS (high security) password.</li>
-            <li>Any such e-mail/SMS or phone call is an attempt to fraudulently withdraw money from your account through Internet Banking. Never respond to such email/SMS or phone call. </li>
-            <li>Change your Internet Banking password at periodical intervals.</li>
-            <li>Always check the last log-in date and time in the post login page.</li>
         </ul>
        </div>
     </div>
